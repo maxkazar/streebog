47539bfcba097786f40f2ce91814a809b16c2b0d
Maxim <maxkazargm@gmail.com>
2023-06-29 15:25:45 +0300

    Fix multiple definition of uint512_u

--- gost3411-2012-core.h
+++ gost3411-2012-core.h
@@ -28,7 +28,9 @@
 ALIGN(16) union uint512_u
 {
     unsigned long long QWORD[8];
-} uint512_u;
+};
+
+extern union uint512_u uint512_u;
 
 #include "gost3411-2012-const.h"
 #include "gost3411-2012-precalc.h"
